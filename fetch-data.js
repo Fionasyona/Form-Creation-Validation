10c01e0779845b0dc2e77873a4b0c4782a76c624
Fiona Wambua <fionawambua20@gmail.com>
2025-06-11 15:52:49 +0300

    new changes

--- fetch-data.js
+++ fetch-data.js
@@ -1,11 +1,11 @@
 async function fetchUserData() {
 
-    const apiurl = "https://jsonplaceholder.typicode.com/users";
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
 
     const dataContainer = document.getElementById('api-data');
 
     try{
-        const response = await fetch(apiurl);
+        const response = await fetch(apiUrl);
         const users = await response.json();
 
          dataContainer.innerHTML = '';
